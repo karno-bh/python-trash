9479daae86c3a94050b34d1c5e2a646418bf8795
karno-bh <sergey.mordeev@gmail.com>
2019-04-24 07:40:17 +0300

    some progress

--- game/board.py
+++ game/board.py
@@ -7,8 +7,23 @@ BLUE = 2
 
 class Board(object):
 
-    def __init__(self, width=5, height=5):
-        self.state = FlatMatrix(width, height)
+    def __init__(self, width=5, height=5, state=None):
+        if state:
+            self.state = state
+        else:
+            self.state = FlatMatrix(width, height)
 
-    def can_move(self, x, y):
-        return True
+    def move(self, x, color, perform=True):
+        state = self.state
+        if x < state.height and state.get(x, state.height - 1) == EMPTY:
+            if perform:
+                y = state.height - 1
+                while state.get(x, y) != EMPTY or y < 0:
+                    y -= 1
+                y += 1
+                return state.set(x, y, color)
+            return True
+        return None if perform else False
+
+    def run_terminal(self, terminal):
+        return terminal(self)
--- game/flat_matrix.py
+++ game/flat_matrix.py
@@ -1,9 +1,12 @@
 class FlatMatrix(object):
 
-    def __init__(self, width, height):
+    def __init__(self, width, height, new_state=None):
         self.width = width
         self.height = height
-        self.data = [0 for x in range(width * height)]
+        if new_state:
+            self.data = new_state
+        else:
+            self.data = [0 for x in range(width * height)]
 
     def __idx(self, i, j):
         return j * self.width + i
@@ -11,13 +14,19 @@ class FlatMatrix(object):
     def get(self, i, j):
         return self.data[self.__idx(i, j)]
 
-    def set(self, i, j, val):
-        self.data[self.__idx(i, j)] = val
+    def set(self, i, j, val, clone=True):
+        if clone:
+            new_state = [x for x in self.data]
+            new_state[self.__idx(i, j)] = val
+            return FlatMatrix(self.width, self.height, new_state)
+        else:
+            self.data[self.__idx(i, j)] = val
+            return self
 
     def __str__(self):
-        str = []
+        buf = []
         for j in range(self.height):
             for i in range(self.width):
-                str += "%d " % self.data[self.__idx(i, j)]
-            str += "\n"
-        return ''.join(str)
+                buf += "%d " % self.data[self.__idx(i, j)]
+            buf += "\n"
+        return ''.join(buf)
