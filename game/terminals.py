e5d6ead52a97860674066d3e64fa91b921b6e4c3
karno-bh <sergey.mordeev@gmail.com>
2019-04-28 07:16:59 +0300

    starting gainful line

--- game/terminals.py
+++ game/terminals.py
@@ -6,7 +6,7 @@ def __walk_landscape(state):
     # type: (FlatMatrix) -> Generator[Tuple[int, int], None, None]
     for i in range(state.width):
         j = state.height - 1
-        while True:
+        while j != -1:
             if state.get(i, j) != EMPTY:
                 break
             j -= 1
@@ -22,10 +22,11 @@ def winner(board, expected_in_line):
         board.state.height,
         [x << indicators_length for x in board.state.data]
     )
+    max_horizontal = state_clone.width - 1
+    max_vertical = state_clone.height - 1
     for i, j in __walk_landscape(state_clone):
-        # j -= 1
-        # if j < 0:
-        #     continue
+        if j == -1:
+            continue
         start_val = state_clone.get(i, j)
         start_real_val = start_val >> indicators_length
         for directions_idx in range(indicators_length):
@@ -39,8 +40,6 @@ def winner(board, expected_in_line):
             for direction in directions:
                 run_i = i
                 run_j = j
-                max_horizontal = board.state.width - 1
-                max_vertical = board.state.height - 1
                 while True:
                     run_i += direction[0]
                     run_j += direction[1]
@@ -48,16 +47,40 @@ def winner(board, expected_in_line):
                         break
                     inspected_element = state_clone.get(run_i, run_j)
                     real_inspected_el_val = inspected_element >> indicators_length
-                    if start_real_val == real_inspected_el_val:
-                        if inspected_element & mask:
-                            raise RuntimeError("Direction had to be checked in previous iterations")
-                        collected_length += 1
-                        state_clone.set(run_i, run_j, inspected_element << indicators_length | mask, clone=False)
-                        if collected_length == expected_in_line:
-                            return real_inspected_el_val
-                    else:
+                    if start_real_val != real_inspected_el_val:
                         break
+                    if inspected_element & mask:
+                        raise RuntimeError("Direction had to be checked in previous iterations")
+                    collected_length += 1
+                    state_clone.set(run_i, run_j, inspected_element << indicators_length | mask, clone=False)
+                    if collected_length == expected_in_line:
+                        return real_inspected_el_val
     return EMPTY
+
+
+def gainful_lines(board, expected_in_line):
+    """
+    Gainful line is a line that can grow till the maximal number of elements by one element on iteration
+    :rtype: Dict
+    """
+    indicators_length = len(DIRECTIONS_PAIRS)
+    state_clone = FlatMatrix(
+        board.state.width,
+        board.state.height,
+        [x << indicators_length for x in board.state.data]
+    )
+    max_horizontal = state_clone.width - 1
+    max_vertical = state_clone.height - 1
+    result = {
+        RED: {},
+        BLUE: {}
+    }
+    for i, j in __walk_landscape(state_clone):
+        j += 1
+        if j > max_vertical:
+            continue
+
+        pass
 
 
 def __test_walk_landscape():
@@ -74,7 +97,7 @@ def __test_walk_landscape():
 
 def __test_win_state():
     data1 = [
-        1, 3, 1, 1,
+        1, 0, 1, 1,
         1, 0, 1, 3,
         1, 0, 0, 3,
         0, 0, 0, 0,
@@ -85,10 +108,10 @@ def __test_win_state():
         3, 3, 1, 3,
         0, 0, 0, 0,
     ]
-    m = FlatMatrix(4, 4, data2)
+    m = FlatMatrix(4, 4, data1)
     print m
     b = Board(4, 4, m)
-    v1 = winner(b, 3)
+    v1 = winner(b, 4)
     print v1
 
 
